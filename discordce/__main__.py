d1b11a13ecc2a2387d56b0859a735dfc729a5830
Slyyxp <11709831+Slyyxp@users.noreply.github.com>
2020-10-26 07:01:19 +0000

    Added verify_cache() function. - Checks for the existence of the discord cache in the default location.

--- discordce/__main__.py
+++ discordce/__main__.py
@@ -1,15 +1,23 @@
 import argparse
+import os
 
 
 def get_args():
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-o', '--output', nargs="*", help="URL.", required=True)
+	parser.add_argument('-o', '--output', nargs="*", help="URL.", required=False)
 	return parser.parse_args()
 
+# Verify that the cache folder exists.
+def verify_cache():
+	path = os.path.join(os.getenv("APPDATA"), "discord", "Cache")
+	if os.path.exists(path):
+		return path
+	else:
+		return None
 
 def main():
 	args = get_args()
-
+	cache = verify_cache()
 
 if __name__ == '__main__':
 	main()
