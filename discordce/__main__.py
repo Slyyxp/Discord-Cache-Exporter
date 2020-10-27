27432eee9ebbaac3dc7b75643f22c7179fdffd7f
Slyyxp <11709831+Slyyxp@users.noreply.github.com>
2020-10-27 08:59:20 +0000

    Functional version.

--- discordce/__main__.py
+++ discordce/__main__.py
@@ -1,5 +1,8 @@
 import argparse
 import os
+from shutil import copy
+
+from discordce import exceptions
 
 
 def get_args():
@@ -7,17 +10,35 @@ def get_args():
 	parser.add_argument('-o', '--output', nargs="*", help="URL.", required=False)
 	return parser.parse_args()
 
-# Verify that the cache folder exists.
+
 def verify_cache():
 	path = os.path.join(os.getenv("APPDATA"), "discord", "Cache")
 	if os.path.exists(path):
 		return path
 	else:
-		return None
+		raise exceptions.CacheNotFound("Cache was not found in the expected location: {}".format(path))
+
+
+def export(cache_folder, output_folder):
+	if output_folder is None:
+		export_path = os.getcwd()
+	else:
+		export_path = output_folder[0]
+	print(export_path)
+	for file in os.listdir(cache_folder):
+		if file.startswith("data"):
+			pass
+		elif file.startswith("index"):
+			pass
+		else:
+			copy(os.path.join(cache_folder, file), os.path.join(export_path, "{}.png".format(file)))
+
 
 def main():
 	args = get_args()
-	cache = verify_cache()
+	cache_folder = verify_cache()
+	export(cache_folder, args.output)
+
 
 if __name__ == '__main__':
 	main()
